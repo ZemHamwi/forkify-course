e2085d5cec5d50d365165ca5821768a66efcede0
ZemHamwi <hazemhamwi98@gmail.com>
2022-05-29 21:18:12 +0300

    added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,6 +126,10 @@ const controlAddRecipe = async function (newRecipe) {
   // upload the new recipe data
 };
 
+const newFeature = function () {
+  console.log("welcome to the application");
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeViews.addHandlerRender(controlRecipes);
@@ -134,6 +138,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log(`initing!`);
+  newFeature();
 };
 init();
