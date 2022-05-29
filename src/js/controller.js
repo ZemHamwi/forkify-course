520fc4d92f6340180367ca09e7bc2e7fd692fd29
ZemHamwi <hazemhamwi98@gmail.com>
2022-05-29 22:15:11 +0300

    cleaned some code

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,10 +126,6 @@ const controlAddRecipe = async function (newRecipe) {
   // upload the new recipe data
 };
 
-const newFeature = function () {
-  console.log("welcome to the application");
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeViews.addHandlerRender(controlRecipes);
@@ -138,6 +134,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
